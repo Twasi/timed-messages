9da08d2fdaafc9af05cea5a2b5ea02e47ae8b53d
NikKasyan <niki.kasyan@gmail.com>
2019-07-01 14:12:57 +0200

    + Startup for UserPlugin

--- src/main/java/at/saith/twasi/TimedMessagesPlugin.java
+++ src/main/java/at/saith/twasi/TimedMessagesPlugin.java
@@ -4,9 +4,12 @@ import at.saith.twasi.service.TimerService;
 import net.twasi.core.plugin.TwasiPlugin;
 import net.twasi.core.plugin.api.TwasiUserPlugin;
 import net.twasi.core.services.ServiceRegistry;
+import net.twasi.core.services.providers.config.ConfigService;
 
 
 public class TimedMessagesPlugin extends TwasiPlugin {
+    public static final String COMMAND_PREFIX = ServiceRegistry.get(ConfigService.class).getCatalog().bot.prefix;
+    public static final TimerService SERVICE = ServiceRegistry.get(TimerService.class);
     @Override
     public void onActivate() {
         ServiceRegistry.register(new TimerService());
--- src/main/java/at/saith/twasi/TimedMessagesUserPlugin.java
+++ src/main/java/at/saith/twasi/TimedMessagesUserPlugin.java
@@ -1,8 +1,16 @@
 package at.saith.twasi;
 
 import at.saith.twasi.cmd.TimerCommand;
+import net.twasi.core.events.TwasiEventHandler;
 import net.twasi.core.plugin.api.TwasiUserPlugin;
+import net.twasi.core.plugin.api.events.TwasiDisableEvent;
+import net.twasi.core.plugin.api.events.TwasiEnableEvent;
 import net.twasi.core.plugin.api.events.TwasiInstallEvent;
+import net.twasi.core.services.ServiceRegistry;
+import net.twasiplugin.dependency.streamtracker.StreamTrackerService;
+import net.twasiplugin.dependency.streamtracker.events.StreamStopEvent;
+import net.twasiplugin.dependency.streamtracker.events.StreamTrackEvent;
+
 
 public class TimedMessagesUserPlugin extends TwasiUserPlugin {
     public TimedMessagesUserPlugin() {
@@ -23,5 +31,30 @@ public class TimedMessagesUserPlugin extends TwasiUserPlugin {
         e.getModeratorsGroup().removeKey("twasi.timer.list");
         e.getModeratorsGroup().removeKey("twasi.timer.enable");
         e.getModeratorsGroup().removeKey("twasi.timer.disable");
+    }
+
+    @Override
+    public void onEnable(TwasiEnableEvent e) {
+        StreamTrackerService sts = ServiceRegistry.get(StreamTrackerService.class);
+        sts.registerStreamTrackEvent(getTwasiInterface().getStreamer().getUser(), new StreamTrackerService.TwasiStreamTrackEventHandler(){
+
+            @Override
+            public void on(StreamTrackEvent streamTrackEvent) {
+                if(!TimedMessagesPlugin.SERVICE.hasTimersEnabled(streamTrackEvent.getUser())){
+                    TimedMessagesPlugin.SERVICE.startTimers(TimedMessagesUserPlugin.this);
+                }
+            }
+        });
+        sts.registerStreamStopEvent(getTwasiInterface().getStreamer().getUser(), new TwasiEventHandler<StreamStopEvent>() {
+            @Override
+            public void on(StreamStopEvent streamStopEvent) {
+                TimedMessagesPlugin.SERVICE.stopTimers(TimedMessagesUserPlugin.this);
+            }
+        });
+    }
+
+    @Override
+    public void onDisable(TwasiDisableEvent e) {
+        TimedMessagesPlugin.SERVICE.stopTimers(this);
     }
 }
