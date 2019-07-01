b71ab2a687b682191b7bae93248fd3cf06dadbf2
NikKasyan <niki.kasyan@gmail.com>
2019-07-01 13:39:57 +0200

    + More Exceptions for better exceptions handling

---
+++ src/main/java/at/saith/twasi/service/exception/CommandNotFoundException.java
@@ -0,0 +1,10 @@
+package at.saith.twasi.service.exception;
+
+public class CommandNotFoundException extends TimerException {
+    public CommandNotFoundException() {
+    }
+
+    public CommandNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/at/saith/twasi/service/exception/NotAllowedTimerException.java
@@ -0,0 +1,7 @@
+package at.saith.twasi.service.exception;
+
+public class NotAllowedTimerException extends TimerException{
+    public NotAllowedTimerException(String message) {
+        super(message);
+    }
+}
