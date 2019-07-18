a0bdeee738daf585014d68e699750d77cce81a2e
NikKasyan <niki.kasyan@gmail.com>
2019-07-18 19:05:44 +0200

    Code Reformatting

--- src/main/java/at/saith/twasi/util/TimeFormatter.java
+++ src/main/java/at/saith/twasi/util/TimeFormatter.java
@@ -1,23 +1,23 @@
 package at.saith.twasi.util;
 
 public class TimeFormatter {
-    public static String formatTime(long timeInSeconds){
-        int days = (int) (timeInSeconds/(60*60*24));
-        int hours = (int) (timeInSeconds/(60*60))%60;
-        int minutes = (int) (timeInSeconds/(60))%60;
-        int seconds = (int) timeInSeconds%60;
+    public static String formatTime(long timeInSeconds) {
+        int days = (int) (timeInSeconds / (60 * 60 * 24));
+        int hours = (int) (timeInSeconds / (60 * 60)) % 60;
+        int minutes = (int) (timeInSeconds / (60)) % 60;
+        int seconds = (int) timeInSeconds % 60;
         StringBuilder formattedTime = new StringBuilder();
-        if(days != 0){
-            formattedTime.append(days+"d");
+        if (days != 0) {
+            formattedTime.append(days + "d");
         }
-        if(hours != 0){
-            formattedTime.append(hours+"h");
+        if (hours != 0) {
+            formattedTime.append(hours + "h");
         }
-        if(minutes != 0){
-            formattedTime.append(minutes+"m");
+        if (minutes != 0) {
+            formattedTime.append(minutes + "m");
         }
-        if(seconds != 0){
-            formattedTime.append(seconds+"s");
+        if (seconds != 0) {
+            formattedTime.append(seconds + "s");
         }
         return formattedTime.toString();
     }
